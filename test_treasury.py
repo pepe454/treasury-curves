1902050a3e6b79b5b752a1d1138b3968ab5aefe3
pepe454 <17fryerd@gmail.com>
2022-09-21 09:40:33 -0500

    add curves() test to test date range functionality

--- test_treasury.py
+++ test_treasury.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -7,20 +9,47 @@ import treasury
 
 @pytest.fixture
 def curve_data():
-    date_range = pd.date_range(end="2020-02-01", periods=40, freq="12M")
+    # all days in the past 30 years (365 * 30 = 10950)
+    date_range = pd.date_range(end="2020-02-15", periods=10950, freq="D")
     curve_data = pd.DataFrame(data={"Date": date_range})
-    curve_data["Year"] = pd.to_datetime(curve_data["Date"])
-    curve_data = curve_data.set_index("Year")
-    curve_data.index = curve_data.index.year
-    curve_data["2 YR"] = 0.5
-    curve_data["5 YR"] = 0.75
-    curve_data["10 YR"] = 1
-    curve_data["20 YR"] = 1.1
-    curve_data["30 YR"] = 1.5
+    curve_data["Date"] = pd.to_datetime(curve_data["Date"])
+    curve_data = curve_data.set_index("Date")
+    curve_data["1 Yr"] = 0.4
+    curve_data["2 Yr"] = 0.5
+    curve_data["5 Yr"] = 0.75
+    curve_data["10 Yr"] = 1
+    curve_data["20 Yr"] = 1.1
+    curve_data["30 Yr"] = 1.5
     return curve_data
 
 
+def test_curves(curve_data):
+    # mock download to guarantee consistency
+    original_download = treasury.download
+    treasury.download = mock.MagicMock(return_value=curve_data)
+    max_date = curve_data.index.max()
+    test_date = max_date.strftime(treasury.DATE_FMT)
+    curves = treasury.curves(test_date, False)
+
+    # curves should group data by year and randomly select 1 sample per year
+    assert curves.index.is_unique
+
+    # dates should be around the time of the testing date
+    curves["Date"] = pd.to_datetime(curves["Date"])
+    day_min, day_max = max_date.day - 5, max_date.day + 5
+    assert np.all(curves["Date"].dt.day < day_max)
+    assert np.all(curves["Date"].dt.day > day_min)
+    assert np.all(curves["Date"].dt.month == max_date.month)
+
+    # un-mock the download
+    treasury.download = original_download
+
+
 def test_filter_curves(curve_data):
+    curve_data = curve_data.reset_index()
+    curve_data["Year"] = curve_data["Date"].dt.year
+    curve_data = curve_data.set_index("Year")
+
     # use num_years alone
     for years in [1,2,3,4,5,6,7,8]:
         numyears_only = treasury.filter_curves(curve_data, num_years=years)
